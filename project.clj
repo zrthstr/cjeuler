b9488e4bc7337b660ab06494e2a360f07c31a6cb
Your Name <you@example.com>
2018-09-01 19:16:59 +0200

    added some dokuentatyion

---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject clojure-getting-started "1.0.0-SNAPSHOT"
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                [net.colourcoding/arianna "0.2.1"]])
+
---
+++ test_project.clj
@@ -0,0 +1,18 @@
+(defproject clojure-getting-started "1.0.0-SNAPSHOT"
+;  :description "testing"
+;  :url "http://clojure-getting-started.herokuapp.com"
+;  :license {:name "Eclipse Public License v1.0"
+;            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                [net.colourcoding/arianna "0.2.1"]
+;                 [compojure "1.1.8"]
+;                 [ring/ring-jetty-adapter "1.2.2"]
+;                 [environ "0.5.0"]
+                    ]
+;  :min-lein-version "2.0.0"
+;  :plugins [[environ/environ.lein "0.2.1"]]
+;  :hooks [environ.leiningen.hooks]
+;  :uberjar-name "clojure-getting-started-standalone.jar"
+;  :profiles {:production {:env {:production true}}}
+        )
+
